e9f83f1a2eed41e2876c3f96d75d24d8e41ab0d3
Peize Sun <40374966+PeizeSun@users.noreply.github.com>
2021-01-12 19:18:06 +0800

    release code

--- models/deformable_detrtrack_train.py
+++ models/deformable_detrtrack_train.py
@@ -277,12 +277,8 @@ class DeformableDETR(nn.Module):
             
         query_embeds = None
         if not self.two_stage:
-            query_embeds = self.query_embed.weight
-        
-        if torch.randn(1).item() > 0.0:
-            hs, init_reference, inter_references, enc_outputs_class, enc_outputs_coord_unact, _ = self.transformer(srcs, masks, pos, query_embeds, pre_reference, pre_tgt)
-        else:
-            hs, init_reference, inter_references, enc_outputs_class, enc_outputs_coord_unact, _ = self.transformer(srcs, masks, pos, query_embeds)            
+            query_embeds = self.query_embed.weight        
+        hs, init_reference, inter_references, enc_outputs_class, enc_outputs_coord_unact, _ = self.transformer(srcs, masks, pos, query_embeds, pre_reference, pre_tgt)           
             
         outputs_classes = []
         outputs_coords = []
