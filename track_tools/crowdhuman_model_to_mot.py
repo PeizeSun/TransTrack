18a2abf2e1e01db775dfbc72a1e6ae9323c5bbec
Peize Sun <sunpeize@foxmail.com>
2021-04-16 09:10:15 +0800

    convert model

--- track_tools/crowdhuman_model_to_mot.py
+++ track_tools/crowdhuman_model_to_mot.py
@@ -1,6 +1,12 @@
 import torch
+import argparse
 
-pre_model = 'output_crowdhuman/checkpoint.pth'
+parser = argparse.ArgumentParser()
+parser.add_argument('--pre_model', default='output_crowdhuman/checkpoint.pth', type=str)
+parser.add_argument('--saved_model', default='crowdhuman_final.pth', type=str)
+args = parser.parse_args()
+
+pre_model = args.pre_model
 aa = torch.load(pre_model)
 
 keys_name = []
@@ -11,4 +17,4 @@ for key in keys_name:
     if key != 'model':
         bb = aa.pop(key)
         
-torch.save(aa, 'crowdhuman_final.pth')
+torch.save(aa, args.saved_model)
