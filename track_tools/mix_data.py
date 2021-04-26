a117ebe36dac53ed430e248e320ddc3eb60b8679
Peize Sun <sunpeize@foxmail.com>
2021-04-26 13:19:14 +0800

    mix crowdhuman&mot

---
+++ track_tools/mix_data.py
@@ -0,0 +1,70 @@
+import json
+import os
+
+# mix_path = 'mix/annotations'
+# if not os.path.exists(mix_path):
+#     os.makedirs(mix_path)
+
+# os.system('cp mot/annotations/val_half.json mix/annotations/val_half.json')
+# os.system('cp mot/annotations/test.json mix/annotations/test.json')
+# os.system('ln -s mot/train mix/mot_train')
+# os.system('ln -s crowdhuman/CrowdHuman_train mix/crowdhuman_train')
+
+"""
+mkdir -p mix/annotations
+cp mot/annotations/val_half.json mix/annotations/val_half.json
+cp mot/annotations/test.json mix/annotations/test.json
+cd mix
+ln -s ../mot/train mot_train
+ln -s ../crowdhuman/CrowdHuman_train crowdhuman_train
+cd ..
+"""
+
+mot_json = json.load(open('mot/annotations/train.json','r'))
+
+img_list = list()
+for img in mot_json['images']:
+    img['file_name'] = 'mot_train/' + img['file_name']
+    img_list.append(img)
+
+ann_list = list()
+for ann in mot_json['annotations']:
+    ann_list.append(ann)
+
+video_list = mot_json['videos']
+category_list = mot_json['categories']
+
+
+
+
+max_img = 10000
+max_ann = 2000000
+max_video = 10
+
+crowdhuman_json = json.load(open('crowdhuman/annotations/train.json','r'))
+img_id_count = 0
+for img in crowdhuman_json['images']:
+    img_id_count += 1
+    img['file_name'] = 'crowdhuman_train/' + img['file_name']
+    img['frame_id'] = img_id_count
+    img['prev_image_id'] = img['id'] + max_img
+    img['next_image_id'] = img['id'] + max_img
+    img['id'] = img['id'] + max_img
+    img['video_id'] = max_video
+    img_list.append(img)
+    
+for ann in crowdhuman_json['annotations']:
+    ann['id'] = ann['id'] + max_ann
+    ann['image_id'] = ann['image_id'] + max_img
+    ann_list.append(ann)
+
+video_list.append({
+    'id': max_video,
+    'file_name': 'crowdhuman'
+})
+mix_json = dict()
+mix_json['images'] = img_list
+mix_json['annotations'] = ann_list
+mix_json['videos'] = video_list
+mix_json['categories'] = category_list
+json.dump(mix_json, open('mix/annotations/train.json','w'))
