bf8dbd159cf91a0448b718e17d75a440c2131c60
Peize Sun <sunpeize@foxmail.com>
2021-07-31 14:01:13 +0800

    update reid_loss_coef

--- main_track.py
+++ main_track.py
@@ -106,7 +106,7 @@ def get_args_parser():
     parser.add_argument('--bbox_loss_coef', default=5, type=float)
     parser.add_argument('--giou_loss_coef', default=2, type=float)
     parser.add_argument('--focal_alpha', default=0.25, type=float)
-    parser.add_argument('--id_loss_coef', default=2, type=float)
+    parser.add_argument('--id_loss_coef', default=1, type=float)
 
     # dataset parameters
     parser.add_argument('--dataset_file', default='coco')
